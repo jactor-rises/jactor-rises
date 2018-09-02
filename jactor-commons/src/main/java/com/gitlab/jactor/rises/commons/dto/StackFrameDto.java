b6fb2fce3cdcc3eff4701718b52561234acf3294
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2018-09-02 12:03:36 +0200

    issue #18: stack walking finished with functional interfaces replacing any log dependency...

--- jactor-commons/src/main/java/com/gitlab/jactor/rises/commons/dto/StackFrameDto.java
+++ jactor-commons/src/main/java/com/gitlab/jactor/rises/commons/dto/StackFrameDto.java
@@ -1,56 +1,33 @@
 package com.gitlab.jactor.rises.commons.dto;
 
-import java.io.Serializable;
+public class StackFrameDto {
 
-public class StackFrameDto implements Serializable {
-    private String className;
-    private String fileName;
-    private String methodName;
+    private final String className;
+    private final String fileName;
+    private final String methodName;
     private int lineNumber;
 
-    @Override public String toString() {
-        return String.format("%s.%s(%s:%d)", getClassName(), getMethodName(), getFileName(), getLineNumber());
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
+    private StackFrameDto(String className, String fileName, int lineNumber, String methodName) {
         this.className = className;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
         this.fileName = fileName;
-    }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public void setMethodName(String methodName) {
         this.methodName = methodName;
-    }
-
-    public int getLineNumber() {
-        return lineNumber;
-    }
-
-    public void setLineNumber(int lineNumber) {
         this.lineNumber = lineNumber;
     }
 
-    public static StackFrameDto from(StackWalker.StackFrame stackFrame) {
-        StackFrameDto stackFrameDto = new StackFrameDto();
-        stackFrameDto.setClassName(stackFrame.getClassName());
-        stackFrameDto.setFileName(stackFrame.getFileName());
-        stackFrameDto.setLineNumber(stackFrame.getLineNumber());
-        stackFrameDto.setMethodName(stackFrame.getMethodName());
+    public String asString() {
+        return String.format("%s.%s(%s:%d)", className, methodName, fileName, lineNumber);
+    }
 
-        return stackFrameDto;
+    @Override public String toString() {
+        return asString();
+    }
+
+    public static StackFrameDto from(StackWalker.StackFrame stackFrame) {
+        return new StackFrameDto(
+                stackFrame.getClassName(),
+                stackFrame.getFileName(),
+                stackFrame.getLineNumber(),
+                stackFrame.getMethodName()
+        );
     }
 }
